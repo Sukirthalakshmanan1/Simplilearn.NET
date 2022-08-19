ca2cda82abf9e964f4a3b1fbf5642bffac651f19
Sukirtha Lakshmanan <80877212+Sukirthalakshmanan1@users.noreply.github.com>
2022-08-19 11:20:08 +0530

    Login program
    Assignment Login program completed.

---
+++ Assignment_Login.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login
+{
+    internal class Assignment_Login
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Firstname which acts as  a Username:");
+            string s = Console.ReadLine();
+            Console.WriteLine("Enter Lastname which acts as  password:");
+            string pass = Console.ReadLine();
+            if (s == "SukirthaLakshmanan" && pass == "Sukilax")
+            {
+                Console.WriteLine("Success!!! " +s +" Welcome to my app");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect Username or Password Kindly check!!");
+            }
+            Console.ReadKey();
+        
+    }
+    }
+}
