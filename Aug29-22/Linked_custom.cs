05f2f5579b9c92dbf706636189ca291458af01b9
Sukirthalakshmanan <sukirtha3700@gmail.com>
2022-08-30 00:07:47 +0530

    Stack assignment

---
+++ Aug29-22/Linked_custom.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_generic_classess
+{
+    internal class Linked_custom
+    {
+        static void Main(string[] args)
+        {
+             UserStack<int> a = new UserStack<int>();
+             
+            Console.WriteLine("Stack Empty= " + a.isStackEmpty());
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("To pop:");
+            a.Pop();
+            Console.WriteLine("---------------------------");
+
+            Console.WriteLine("Enter no of elements to be added in stack:");
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter {0} elements :", n);
+            for (int i = 0; i < n; i++)
+            {
+                int a1 = Convert.ToInt32(Console.ReadLine());
+                a.Push(a1);
+            }
+            Console.WriteLine("-----------------------------");
+
+            Console.WriteLine("Elements in Stack:");
+            a.PrintStackElements();
+
+            Console.WriteLine("---------------------");
+            Console.WriteLine("Popped element: ");
+            a.Pop();
+            Console.WriteLine("---------------------");
+            Console.WriteLine("Peek element:");
+            a.Peek();
+            Console.WriteLine("---------------------");
+            Console.WriteLine("Elements in Stack:");
+            a.PrintStackElements();
+            Console.WriteLine("Stack Empty= " + a.isStackEmpty());
+
+
+            Console.ReadKey();
+        }
+    }
+    }
+
---
+++ Aug29-22/Stack_generice_1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_generic_classess
+{
+    class UserStack<T>
+    {
+        internal static readonly int max = 1000;
+        T[] stack = new T[max];
+        int top;
+
+        internal bool isStackEmpty()
+        {
+
+            return (top < 0);
+        }
+
+        public UserStack()
+        {
+            top = -1;
+        }
+
+        public void Push(T data)
+        {
+            if (top >= max)
+            {
+                Console.WriteLine("No space on the stack");
+                return;
+            }
+            else
+            {
+                stack[++top] = data;
+
+            }
+        }
+
+        public void Pop()
+        {
+            //int value = 0;
+            if (top < 0)
+            {
+                Console.WriteLine("No elements on the stack");
+                return;
+            }
+            else
+            {
+                T value = stack[top--];
+                Console.WriteLine(value);
+            }
+        }
+        public void Peek()
+        {
+            if (top < 0)
+            {
+                Console.WriteLine("No elements on the stack");
+                return;
+            }
+            else
+            {
+                T value = stack[top];
+                Console.WriteLine(value);
+            }
+        }
+
+        internal void PrintStackElements()
+        {
+            if (top < 0)
+            {
+                Console.WriteLine("No elements on the stack");
+                return;
+            }
+            else
+            {
+                for (int i = top; i >= 0; i--)
+                {
+                    Console.WriteLine(stack[i]);
+                }
+            }
+        }
+    }
+}
