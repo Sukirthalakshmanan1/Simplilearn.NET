8732eb32a575b99de2707fc70dcf14ce56de1305
Sukirthalakshmanan <sukirtha3700@gmail.com>
2022-08-30 00:09:15 +0530

    Linklist assignment

---
+++ Aug29-22/Linklist_Generic1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_generic_classess
+{
+    internal class Linklist_Generic1
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Custom generic Linklist Implementation :");
+            linkedList_1<int> a = new linkedList_1<int>();
+            int a1 = 50;
+            int b = 200;
+            int c = 100;
+
+            a.AddNode(a1);
+            a.AddNode(b);
+            a.AddNode(c);
+
+
+
+            foreach (int item in a)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-------------------------------------------");
+
+
+            linkedList_1<string> list = new linkedList_1<string>(); 
+            string s1 = "Jose";
+            string s2 = "Jam";
+            string s3 = "Jill";
+            string s4 = "Jack";
+
+            list.AddNode(s1);
+            list.AddNode(s2);
+            list.AddNode(s3);
+            list.AddNode(s4);
+
+
+            foreach (string s in list)
+            {
+                Console.WriteLine(s);
+            }
+
+            
+
+            Console.ReadKey();
+
+
+        }
+    }
+
+
+
+}
---
+++ Aug29-22/generic_singly1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_generic_classess
+{
+    class linkedList_1<T>
+    {
+        class LinkedNode
+        {
+            private LinkedNode next;
+            private T data;
+
+            public LinkedNode(T t)
+            {
+                next = null;
+                data = t;
+
+            }
+            public LinkedNode Next
+            {
+                get
+                {
+                    return next;
+                }
+                set
+                {
+                    next = value;
+                }
+            }
+            public T Data
+
+            {
+                get
+                {
+                    return data;
+                }
+                set
+                {
+                    data = value;
+                }
+            }
+        }
+        private LinkedNode head;
+        public linkedList_1()
+        {
+            head = null;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            LinkedNode current = head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+
+        }
+        public void AddNode(T t)
+        {
+            LinkedNode node = new LinkedNode(t);
+            node.Next = head;
+            head = node;
+        }
+        
+
+    }
+
+}
