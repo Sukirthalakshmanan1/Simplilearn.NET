4bd43ffcf4a4e6c21da01385edeba9bbb5d55b70
Sukirthalakshmanan <sukirtha3700@gmail.com>
2022-09-12 23:08:52 +0530

    reflection

---
+++ Sept12-22/Reflection/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"G:\C#\Sep12\Calculator_1\calculator\bin\Debug\calculator.dll";
+            Assembly asm = Assembly.LoadFrom(path);
+            Type[] types = asm.GetTypes();
+            Type t = null;
+            
+            foreach(var item in types)
+            {
+                t = asm.GetType(item.FullName);
+                Console.WriteLine("To find Cube of a number:");
+                Console.WriteLine("---------------------------");
+                Console.WriteLine("Enter a number:");
+                double n=Convert.ToInt32(Console.ReadLine());
+                object obj = Activator.CreateInstance(t);
+                item.InvokeMember("cube", BindingFlags.Public | BindingFlags.Instance | BindingFlags.InvokeMethod, null, obj, new object[1] { n });
+                Console.WriteLine("---------------------------");
+                Console.WriteLine("To find Sqrt of a number:");
+                Console.WriteLine("---------------------------");
+                Console.WriteLine("Enter a number:");
+                double n1 = Convert.ToInt32(Console.ReadLine());
+                item.InvokeMember("sqrt", BindingFlags.Public | BindingFlags.Instance | BindingFlags.InvokeMethod, null, obj, new object[1] { n1 });
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Sept12-22/Reflection/simple.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    public class simple
+    {
+        private double i;
+
+        public double num1
+        {
+            get { return i; }
+            set { i = value; }
+        }
+
+        
+        public void cube(double i)
+        {
+            double a = i * i * i;
+            //return a;
+            Console.WriteLine("Cube of {0} = {1}",i,a);
+        }
+
+        public void sqrt(double i)
+        {
+            double s = Math.Sqrt(i);
+            Console.WriteLine("Sqrt of {0} = {1}",i,s);
+        }
+    }
+}
